fb6133707e0335b4b46de242b91f8aa9572c5d31
TheRealMct21 <85460336+TheRealMct21@users.noreply.github.com>
2021-11-30 19:01:40 -0800

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -12,9 +12,11 @@ import * as MRE from '@microsoft/mixed-reality-extension-sdk';
 export default class HelloWorld {
 	private text: MRE.Actor = null;
 	private button: MRE.Actor = null;
+	private button1: MRE.Actor = null;
 	private player: MRE.Actor = null;
 	private assets: MRE.AssetContainer;
 	private buzzerSound: MRE.Sound = undefined;
+	private buzzerSound1: MRE.Sound = undefined;
 
 	constructor(private context: MRE.Context) {
 		this.context.onStarted(() => this.started());
@@ -49,15 +51,41 @@ export default class HelloWorld {
 				}
 			}
 		});
+		
+		
+		
+		
+		this.button1 = MRE.Actor.CreateFromPrefab(this.context, {
+			// using the data we loaded earlier
+			firstPrefabFrom: cubeData,
+			// Also apply the following generic actor properties.
+			actor: {
+				name: 'Altspace button',
+				// Parent the glTF model to the text actor, so the transform is relative to the text
+				parentId: menu.id,
+				transform: {
+					local: {
+						position: { x: 1: y: -1, z: 0 },
+						scale: { x: 0.4, y: 0.4, z: 0.4 }
+					}
+				}
+			}
+		});
 
 		this.buzzerSound = this.assets.createSound(
-			'alarmSound',
+			'wrong1',
+			{ uri: 'https://cdn-content-ingress.altvr.com/uploads/audio_clip/audio/1882339059980829529/ogg_buzzersound.ogg' });
+		
+		this.buzzerSound1 = this.assets.createSound(
+			'buzz1',
 			{ uri: 'https://cdn-content-ingress.altvr.com/uploads/audio_clip/audio/1882339059980829529/ogg_buzzersound.ogg' });
 
 
 		// Set up cursor interaction. We add the sound
 		// Button behaviors have two pairs of events: hover start/stop, and click start/stop.
 		const buttonBehavior = this.button.setBehavior(MRE.ButtonBehavior);
+		
+		const buttonBehavior1 = this.button1.setBehavior(MRE.ButtonBehavior);
 
 		// Trigger the grow/shrink animations on hover.
 		buttonBehavior.onHover('enter', () => {
@@ -68,6 +96,9 @@ export default class HelloWorld {
 				easing: MRE.AnimationEaseCurves.EaseOutSine
 			});
 		});
+		
+		
+		
 		buttonBehavior.onHover('exit', () => {
 			MRE.Animation.AnimateTo(this.context, this.button, {
 				destination: { transform: { local: { scale: { x: 0.4, y: 0.4, z: 0.4 } } } },
@@ -80,23 +111,20 @@ export default class HelloWorld {
 		buttonBehavior.onClick(_ => {
 			
 			this.startSound();
-			MRE.Animation.AnimateTo(this.context, this.button, {
-				destination: { transform: { local: { scale: { x: 2, y: 5, z: 2 } } } },
-				duration: 0.7,
-				easing: MRE.AnimationEaseCurves.EaseOutSine
-			});
-			MRE.Animation.AnimateTo(this.context, this.button, {
-				destination: { transform: { local: { scale: { x: 0.4, y: 0.4, z: 0.4 } } } },
-				duration: 0.3,
-				easing: MRE.AnimationEaseCurves.EaseOutSine
-			});
+			
 		});
 
 	}
 	private startSound = () => {
-		const options: MRE.SetAudioStateOptions = { volume: 0.4 };
+		const options: MRE.SetAudioStateOptions = { volume: 0.1 };
 		options.time = 0;
 		this.button.startSound(this.buzzerSound.id, options);
+	}
+	
+	private startSound1 = () => {
+		const options: MRE.SetAudioStateOptions = { volume: 0.1 };
+		options.time = 0;
+		this.button1.startSound(this.buzzerSound.id, options);
 	}
 
 }
